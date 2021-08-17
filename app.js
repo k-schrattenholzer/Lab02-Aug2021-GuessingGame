c13192abe5ca90592ae4ff7f212b7f8d0e0e2d98
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-08-17 16:58:38 -0700

    moving UI work into functions

--- app.js
+++ app.js
@@ -17,28 +17,28 @@ guessButton.addEventListener ('click', () => {
     console.log(correctAnswer);
     let currentGuess = Number(userGuess.value);
     let result = compareNumbers (currentGuess, correctAnswer);
+
     guessEl.style.display = 'block';
+
     if (result === 0) {
         userWin();
-        disableInputForReplay();
+        disableForReplay();
     }
     if (result === 1) {
-        resultsSpan.style.display = 'flex';
-        resultsSpan.textContent = 'Too high, try again.';
-        console.log ('too high');
+        displayResults();
+        resultTooHigh();
     }
     if (result === -1) {
-        resultsSpan.style.display = 'flex';
-        resultsSpan.textContent = 'Too low, try again.';
-        console.log ('too low');
+        resultTooLow();
+        displayResults();
     }
     if (guessCount === 1 && result !== 0) {
-        resultsSpan.textContent = 'You had four tries but alas, the wizard takes the gold. Try again!';
-        disableInputForReplay();
+        userLoss();
+        disableForReplay();
     }
     if (guessCount === 1 && result === 0) {
         userWin();
-        disableInputForReplay();
+        disableForReplay();
     }
     decrementGuesses();
 });
@@ -47,7 +47,7 @@ resetButton.addEventListener ('click', () => {
     location.reload ();
 });
 
-function disableInputForReplay() {
+function disableForReplay() {
     guessButton.style.display = 'none';
     userGuess.style.display = 'none';
     guessEl.style.display = 'none';
@@ -59,7 +59,23 @@ function userWin() {
     resultsSpan.textContent = 'HOT DOG! You guessed it!';
 }
 
+function userLoss() {
+    resultsSpan.textContent = 'You had four tries but alas, the wizard takes the gold. Try again!';
+}
+
 function decrementGuesses() {
     guessCount --;
     guessRem.textContent = guessCount;
+}
+
+function displayResults() {
+    resultsSpan.style.display = 'block';
+}
+
+function resultTooHigh() {
+    resultsSpan.textContent = 'Too high, try again.';
+}
+
+function resultTooLow() {
+    resultsSpan.textContent = 'Too low, try again.';
 }
